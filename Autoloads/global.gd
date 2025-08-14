78cb5383a49f7005260d670abd842ffc1e5fa284
Groblockia <groblovroum@gmail.com>
2025-08-14 19:16:19 +0200

    wallahi i'm cooked

--- Autoloads/global.gd
+++ Autoloads/global.gd
@@ -4,4 +4,5 @@ func _ready():
 	SignalManager.movement.connect(cacapipi)
 
 func cacapipi():
-	print("c'est le mec il suce le cul d'une pute")
+	pass
+	#print("c'est le mec il suce le cul d'une pute")
--- Player/player.gd
+++ Player/player.gd
@@ -2,7 +2,16 @@ extends Node3D
 var is_moving = false
 
 const MOVE_DISTANCE = 2
+var FORWARD_DIRECTION = -transform.basis.z.normalized()
+var BACKWARD_DIRECTION = transform.basis.z.normalized()
+var LEFT_DIRECTION = -transform.basis.x.normalized()
+var RIGHT_DIRECTION = transform.basis.x.normalized()
 @onready var move_timer = $Move_timer
+
+@onready var ray_forward = $Raycasts/Forward
+@onready var ray_backward = $Raycasts/Backward
+@onready var ray_left = $Raycasts/Left
+@onready var ray_right = $Raycasts/Right
 
 
 func _ready() -> void:
@@ -10,44 +19,178 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	pass
+	FORWARD_DIRECTION = -transform.basis.z.normalized()
+	BACKWARD_DIRECTION = transform.basis.z.normalized()
+	LEFT_DIRECTION = -transform.basis.x.normalized()
+	RIGHT_DIRECTION = transform.basis.x.normalized()
 
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("forward"):
-		move_fwd()
+		move_forward()
 	if Input.is_action_pressed("backward"):
-		move_bwd()
+		move_backward()
+	if Input.is_action_pressed("left"):
+		move_left()
+	if Input.is_action_pressed("right"):
+		move_right()
 	if Input.is_action_pressed("turn_left"):
 		turn_left()
 	if Input.is_action_pressed("turn_right"):
 		turn_right()
 
 
-func move_fwd():
+func move_forward():
 	if !is_moving:
-		SignalManager.movement.emit()
-		is_moving = true
-		var direction = -transform.basis.z.normalized()
-		var tween = create_tween()
-		tween.tween_property(self, "position", position + (direction*MOVE_DISTANCE), 0.2)
-		await tween.finished
-		move_timer.start()
-		await move_timer.timeout
-		is_moving = false
+		
+		#if path is blocked
+		if ray_forward.is_colliding():
+			print("path blocked")
+			is_moving = true
+			
+			var col_point = ray_forward.get_collision_point()
+			var old_pos = position
+			
+			var tween = create_tween()
+			tween.tween_property(self, "position", Vector3(col_point.x,position.y,col_point.z), 0.2)
+			await tween.finished
+			
+			#play bump sound
+			
+			tween = create_tween()
+			tween.tween_property(self, "position", old_pos, 0.2)
+			await tween.finished
+			
+			move_timer.start()
+			await move_timer.timeout
+			is_moving = false
+		
+		#if path is clear
+		else:
+			SignalManager.movement.emit()
+			is_moving = true
+			
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + (FORWARD_DIRECTION*MOVE_DISTANCE), 0.2)
+			await tween.finished
+			
+			move_timer.start()
+			await move_timer.timeout
+			is_moving = false
 
-
-func move_bwd():
+func move_backward():
 	if !is_moving:
-		SignalManager.movement.emit()
-		is_moving = true
-		var direction = transform.basis.z.normalized()
-		var tween = create_tween()
-		tween.tween_property(self, "position", position + (direction*MOVE_DISTANCE), 0.2)
-		await tween.finished
-		move_timer.start()
-		await move_timer.timeout
-		is_moving = false
+		
+		#if path is blocked
+		if ray_backward.is_colliding():
+			print("path blocked")
+			is_moving = true
+			
+			var col_point = ray_backward.get_collision_point()
+			var old_pos = position
+			
+			var tween = create_tween()
+			tween.tween_property(self, "position", Vector3(col_point.x,position.y,col_point.z), 0.2)
+			await tween.finished
+			
+			#play bump sound
+			
+			tween = create_tween()
+			tween.tween_property(self, "position", old_pos, 0.2)
+			await tween.finished
+			
+			move_timer.start()
+			await move_timer.timeout
+			is_moving = false
+		
+		#if path is clear
+		else:
+			SignalManager.movement.emit()
+			is_moving = true
+			
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + (BACKWARD_DIRECTION*MOVE_DISTANCE), 0.2)
+			await tween.finished
+			
+			move_timer.start()
+			await move_timer.timeout
+			is_moving = false
+
+func move_left():
+	if !is_moving:
+		
+		#if path is blocked
+		if ray_left.is_colliding():
+			print("path blocked")
+			is_moving = true
+			
+			var col_point = ray_left.get_collision_point()
+			var old_pos = position
+			
+			var tween = create_tween()
+			tween.tween_property(self, "position", Vector3(col_point.x,position.y,col_point.z), 0.2)
+			await tween.finished
+			
+			#play bump sound
+			
+			tween = create_tween()
+			tween.tween_property(self, "position", old_pos, 0.2)
+			await tween.finished
+			
+			move_timer.start()
+			await move_timer.timeout
+			is_moving = false
+		
+		#if path is clear
+		else:
+			SignalManager.movement.emit()
+			is_moving = true
+			
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + (LEFT_DIRECTION*MOVE_DISTANCE), 0.2)
+			await tween.finished
+			
+			move_timer.start()
+			await move_timer.timeout
+			is_moving = false
+
+func move_right():
+	if !is_moving:
+		
+		#if path is blocked
+		if ray_right.is_colliding():
+			print("path blocked")
+			is_moving = true
+			
+			var col_point = ray_right.get_collision_point()
+			var old_pos = position
+			
+			var tween = create_tween()
+			tween.tween_property(self, "position", Vector3(col_point.x,position.y,col_point.z), 0.2)
+			await tween.finished
+			
+			#play bump sound
+			
+			tween = create_tween()
+			tween.tween_property(self, "position", old_pos, 0.2)
+			await tween.finished
+			
+			move_timer.start()
+			await move_timer.timeout
+			is_moving = false
+		
+		#if path is clear
+		else:
+			SignalManager.movement.emit()
+			is_moving = true
+			
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + (RIGHT_DIRECTION*MOVE_DISTANCE), 0.2)
+			await tween.finished
+			
+			move_timer.start()
+			await move_timer.timeout
+			is_moving = false
 
 
 func turn_left():
@@ -60,7 +203,6 @@ func turn_left():
 		move_timer.start()
 		await move_timer.timeout
 		is_moving = false
-
 
 func turn_right():
 	if !is_moving:
